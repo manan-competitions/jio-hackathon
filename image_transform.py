21984e472a012a33a9970b00ea9fd45e5d59297e
Siddharth Singh <sidharth_0903@hotmail.com>
2019-05-27 22:38:58 +0530

    Added visualisation functions

--- image_transform.py
+++ image_transform.py
@@ -2,6 +2,7 @@ import sys
 from skimage import filters, morphology, feature
 from skimage.transform import (hough_line, hough_line_peaks,
                                probabilistic_hough_line)
+
 from PIL import Image
 import numpy as np
 
@@ -25,3 +26,59 @@ def pre_process(im):
     # get(edges).show()
 #    get(edges).convert('RGB').save('warped_2.jpg')
     return edge_im, edges
+
+
+def hist_segment(im):
+    n = 10
+    l = 256
+    hist, bin_edges = np.histogram(im, bins=256)
+    bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
+    plt.plot(hist)
+    binary_img = im > 0.5
+    return binary_img
+
+def visualize_hsv(im):
+    pixel_colors = im.reshape((np.shape(im)[0]*np.shape(im)[1], 3))
+    norm = colors.Normalize(vmin=-1.,vmax=1.)
+    norm.autoscale(pixel_colors)
+    pixel_colors = norm(pixel_colors).tolist()
+    hsv_im = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
+    h, s, v = cv2.split(hsv_im)
+    fig = plt.figure()
+    axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+    axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+    axis.set_xlabel("Hue")
+    axis.set_ylabel("Saturation")
+    axis.set_zlabel("Value")
+    plt.show()
+
+def show_hsv(im):
+    import matplotlib.pyplot as plt
+
+    from skimage import data
+    from skimage.color import rgb2hsv
+    rgb_img = im
+    hsv_img = rgb2hsv(rgb_img)
+    hue_img = hsv_img[:, :, 0]
+    sat_img = hsv_img[:,:, 1]
+    value_img = hsv_img[:, :, 2]
+
+    fig, (ax0, ax1, ax2, ax3) = plt.subplots(ncols=4, figsize=(8, 2))
+
+    ax0.imshow(rgb_img)
+    ax0.set_title("RGB image")
+    ax0.axis('off')
+    ax1.imshow(hue_img, cmap='hsv')
+    ax1.set_title("Hue channel")
+    ax1.axis('off')
+    ax2.imshow(value_img)
+    ax2.set_title("Value channel")
+    ax2.axis('off')
+    ax3.imshow(sat_img)
+    ax3.set_title("Saturation channel")
+    ax3.axis('off')
+
+    fig.tight_layout()
+    plt.show()
+
